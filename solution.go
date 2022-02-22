04b6b2c050757b29a7a5cb04e8109ac7285918d6
Адильжан Аубакир <aubakirof@gmail.com>
2022-02-22 17:26:32 +0600

    dd

--- solution.go
+++ solution.go
@@ -1,13 +1,9 @@
 package solution
 
 import (
-	"fmt"
 	"github.com/kyokomi/emoji/v2"
 )
 
-func main(){
-	return GetMessage()
-}
 
 func GetMessage() string {
 	return emoji.Sprint("Hello :world_map:")
